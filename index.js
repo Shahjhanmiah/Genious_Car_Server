f2cd8563e6ee96f271353e5d66252445cb295789
mdshahjhan <mdshahjha478877@gmail.com>
2023-04-19 11:05:07 +0600

    serversite

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
 require('dotenv').config()
-
 const port = process.env.Port || 5000;
 
 
@@ -19,21 +17,64 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
- async function run (){
-    try{
+async function run() {
+    const serviceCollection = client.db('Foodpanda').collection('services');
+    const orderCollection = client.db("Foodpanda").collection("orders")
+    try {
+        // serviceCollection.insertMany([{name:"shajhan miah"},{Roll:478877},{school:
+        // 'Victoriay'}])
+
+        // create a data inserte data
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services)
+
+        })
+
+        // speace data loade data 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        // order a get api 
+
+          app.get('/orders',async(req,res)=>{
+            let  query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+
+          })
+        // post api 
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
 
 
     }
-    finally{
-       
+    finally {
+
     }
-   
- }
- run().catch(error=>console.log(error))
+
+}
+run().catch(error => console.log(error))
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("genious car is server is runing")
 
 
@@ -41,7 +82,7 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(` genious car is Server running port${port}`)
 })
  //  user name database
